088bdcec4f96a24e51562bde5d32402d89d2c2c2
Jproga <ilyba3402@mail.ri>
2024-03-13 13:08:38 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -88,6 +88,18 @@ def make_some(message):
     bot.send_message(message.chat.id, 'I accepted a new user!')
     bot.approve_chat_join_request(message.chat.id, message.from_user.id)
 
+@bot.message_handler(func=lambda message: True)
+def auto_ban(message):
+    if message.text == "https://":
+        chat_id = message.chat.id 
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status 
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Не нарушайте правила")
+        else:
+            bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
+            bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
+
 
 
 
